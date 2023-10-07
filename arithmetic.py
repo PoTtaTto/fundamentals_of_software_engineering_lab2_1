cb55444c33a3f2b474269005ed96cc328d61a0d7
MrPotatsus <MrPotatsus@yandex.ru>
2023-10-07 18:57:29 +0300

    Report is added

--- arithmetic.py
+++ arithmetic.py
@@ -2,8 +2,8 @@ def solve_math_problem():
     print('Task: 4 * 100 - 54')
     user_answer = int(input('Enter your answer: '))
     right_answer = 4 * 100 - 54
-    print(f'You\'re answer: {user_answer}\nRight answer: {right_answer}')
+    print(f'Your answer: {user_answer}\nRight answer: {right_answer}')
     if user_answer == right_answer:
         print('You\'re right!')
     else:
-        print('You\'re answer is wrong!')
+        print('Your answer is wrong!')
